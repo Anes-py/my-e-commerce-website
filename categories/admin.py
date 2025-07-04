6ae57cb21ca68091336f6617758be6a97edbfb54
Anes-py <AnesDevpy@gmail.com>
2025-07-04 13:04:10 +0330

    setup category admin

--- categories/admin.py
+++ categories/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name',]
+    list_display_links = ['name',]
+    list_filter = ['name',]
+    search_fields = ['name']
+    list_per_page = 20
+    list_max_show_all = 30
