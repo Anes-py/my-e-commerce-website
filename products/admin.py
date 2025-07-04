d4571824d7dac7670a9abbb598caa6463adc42ea
Anes-py <AnesDevpy@gmail.com>
2025-07-04 14:46:57 +0330

    setup product admin...

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+    min_num = 1
+    max_num = 20
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'id',
+        'price',
+        'stock',
+        'status',
+        'is_active',
+        'created_at',
+        'updated_at',
+    ]
+    ordering = ['stock']
+    list_display_links = ['name']
+    list_per_page = 20
+    list_max_show_all = 30
+    list_editable = ['status', 'is_active']
+    list_filter = ['status', 'is_active', 'category']
+    autocomplete_fields = ['category']
+    search_fields = ['name', 'short_description']
+
+    inlines = [
+        ProductImageInline
+    ]
