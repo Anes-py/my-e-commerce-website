9392be1ea392b3d5783e3a05890d212c41278f05
Anes-py <AnesDevpy@gmail.com>
2025-07-05 22:03:24 +0330

    feat: add search and filtering to product_list_view

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.db.models import Subquery, OuterRef
+from django.db.models import Subquery, OuterRef, Q
+from django.conf import settings
+from django.utils import timezone
 
 from categories.models import Category
 from .models import Product, ProductImage
@@ -19,8 +21,40 @@ def home_view(request):
     return render(request, 'products/home.html', {
         'discounted_products': discounted_products,
         'newest_products': newest_products,
-        'top_categories': top_categories
+        'top_categories': top_categories,
+        'main_image_url_prefix': settings.MEDIA_URL,
         })
 
+
 def product_list_view(request):
-    return render(request, 'products/products_list.html')
+    main_image_subquery = ProductImage.objects.filter(
+        product=OuterRef('pk'),
+        is_main=True,
+    ).values('image')[:1]
+
+    queryset = Product.objects.newest()
+    search_query = request.GET.get('q')
+    filter_query = request.GET.get('f')
+
+    if search_query:
+        queryset = Product.objects.search(search_query)
+
+    if filter_query == 'discounted':
+        queryset = Product.objects.with_discount()
+
+    if filter_query == 'most_expensive':
+        queryset = Product.objects.most_expensive()
+
+    if filter_query == 'cheapest':
+        queryset = Product.objects.cheapest()
+
+    main_image_subquery = ProductImage.objects.filter(
+        product=OuterRef('pk'),
+        is_main=True,
+    ).values('image')[:1]
+    queryset = queryset.annotate(main_image=Subquery(main_image_subquery))
+
+    return render(request, 'products/products_list.html', context={
+        'product_list': queryset,
+        'main_image_url_prefix': settings.MEDIA_URL,
+    })
