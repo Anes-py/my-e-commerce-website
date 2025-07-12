87fbebf9cb2d9a53855a7881c261fa807c8f02a8
Anes-py <AnesDevpy@gmail.com>
2025-07-13 01:03:18 +0330

    update url pattern of home view

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='home'),
+    path('', views.HomeView.as_view(), name='home'),
     path('products/', views.ProductListView.as_view(), name='product-list'),
     path('products/<str:slug>', views.ProductDetailView.as_view(), name='product-detail'),
 ]
